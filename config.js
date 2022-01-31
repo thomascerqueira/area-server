2bcdb06927ffedc50925b8c93ed106bdeefa81be
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-01-31 18:04:42 +0100

    []

--- config.js
+++ config.js
@@ -2,21 +2,15 @@ import { getAuth } from 'firebase-admin/auth';
 import admin from 'firebase-admin'
 import Mongodb from 'mongodb'
 import nodemailer from 'nodemailer'
-console.log("test", process.env.FIREBASE_PRIVATE_KEY)
+
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const myconfigFile = require("./area-e3fc2-firebase-adminsdk-oxpqx-d9f415baae.json")
+
 admin.initializeApp({
-    credential: admin.credential.cert({
-        // "type": process.env.FIREBASE_TYPE,
-        "project_id": process.env.FIREBASE_PROJECT_ID,
-        // "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-        // "client_id": process.env.FIREBASE_CLIENT_ID,
-        // "auth_uri": process.env.FIREBASE_AUTH_URI,
-        // "token_uri": process.env.FIREBASE_TOKEN_URI,
-        // "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER,
-        // "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT
-    })
-  });
+    credential: admin.credential.cert(myconfigFile)
+});
+
 
 const auth = getAuth()
 let allDb
