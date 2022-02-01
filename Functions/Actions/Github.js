8319d9dff06e9b43e78c99d783f1c641f1696dbf
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-01 18:02:47 +0100

    [ADD] global actions

--- Functions/Actions/Github.js
+++ Functions/Actions/Github.js
@@ -1,9 +1,9 @@
 import httpRequest from "../httpRequest.js";
 
-async function createGithubAction({githubName, repository, token}) {
+async function createGithubAction(options) {
   try {
     return await httpRequest(
-      `https://api.github.com/repos/${githubName}/${repository}/hooks`,
+      `https://api.github.com/repos/${options.githubName}/${options.repository}/hooks`,
       "post",
       {
         name: "web",
@@ -11,11 +11,11 @@ async function createGithubAction({githubName, repository, token}) {
         config: {
           url: "https://area-epitech2.herokuapp.com/actions/github/hooks",
           content_type: "json",
-          token: token
+          token: options.token
         },
       },
       {
-        Authorization: `token ${token}`
+        Authorization: `token ${options.token}`
       })
   } catch (err) {
     throw err;
