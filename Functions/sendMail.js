c2a76579ae42820bacbe0e8a07891b33e419353c
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-02-01 11:44:23 +0100

    [UPDATE] confirm mail template

--- Functions/sendMail.js
+++ Functions/sendMail.js
@@ -1,11 +1,17 @@
 import {transporter} from "../config.js";
+import {hbs} from "nodemailer-express-handlebars"
 
-async function sendMail(email, object, body, context) {
+async function sendMail(email, object, template, context) {
+  transporter.use("compile", hbs({
+    viewEngine: "express-handlebars",
+    viewPaths: "../Template/"
+  }))
+
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: email,
     subject: object,
-    html: body,
+    template: template,
     context: context
   }
   transporter.sendMail(mailOptions)
--- Middleware/Auth/auth.js
+++ Middleware/Auth/auth.js
@@ -4,7 +4,6 @@ import {allDb} from "../../config.js";
 import {getOneValueDb} from "../../Functions/MongoDB/getValueDb.js";
 import {sendMail} from '../../Functions/sendMail.js'
 import {userSchema} from "../../user.js";
-import {createReadStream} from "fs"
 
 function createUser(req, res) {
 
@@ -17,7 +16,7 @@ function createUser(req, res) {
       auth.generateEmailVerificationLink(req.body.email)
         .then((value) => {
           sendMail(req.body.email, "Verify your email",
-            createReadStream("/Template/confirm.html"), {CONFIRM_LINK: value, EMAIL_ADRESS: req.body.email})
+            "confirm", {CONFIRM_LINK: value, EMAIL_ADRESS: req.body.email})
             .then(() => {})
         })
       addDocC(allDb[process.env.DB_MONGO_USERS], "users", new userSchema({
