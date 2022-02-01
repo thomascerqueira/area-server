11b0757f5180188d3e174271216d66ffb9ea034a
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-01 18:54:11 +0100

    [ADD] global actions

--- Middleware/Actions/github.js
+++ Middleware/Actions/github.js
@@ -5,17 +5,18 @@ import {getOneValueDb} from "../../Functions/MongoDB/getValueDb.js";
 import {allDb} from "../../config.js";
 
 function getWebHooks(req, res) {
-    console.log(req.headers['x-github-hook-id'])
-    console.log(req.headers)
-    getOneValueDb(allDb['ActionReaction'], '*', {
+    if (req.headers['x-github-event'] !== 'ping') {
+      console.log(req.headers)
+      getOneValueDb(allDb['ActionReaction'], 'ActionReaction', {
         hook_id: req.headers['x-github-hook-id']
-    })
-      .then((res) => {
+      })
+        .then((res) => {
           console.log(res)
-      })
-      .catch((err) => {
+        })
+        .catch((err) => {
           console.error(err)
-      })
+        })
+    }
     res.status(200).send({})
 }
 
