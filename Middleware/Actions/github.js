81a17a77ed2ae0d6d9c2360026bbb512a870c6ec
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-01 17:46:18 +0100

    [ADD] global actions

--- Middleware/Actions/github.js
+++ Middleware/Actions/github.js
@@ -1,10 +1,21 @@
 import {addDocC} from "../../Functions/MongoDB/addDoc.js";
 import httpRequest from "../../Functions/httpRequest.js";
 import axios from "axios"
+import {getOneValueDb} from "../../Functions/MongoDB/getValueDb.js";
+import {allDb} from "../../config.js";
 
 function getWebHooks(req, res) {
     console.log(req.headers['x-github-hook-id'])
     console.log(req.headers)
+    getOneValueDb(allDb['ActionReaction'], '*', {
+        hook_id: req.headers['x-github-hook-id']
+    })
+      .then((res) => {
+          console.log(res)
+      })
+      .catch((err) => {
+          console.error(err)
+      })
     res.status(200).send({})
 }
 
