b9b60968f8899dacb4b38d3effbe619e9335cd0b
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-01 18:28:11 +0100

    [ADD] global actions

--- Middleware/Actions/github.js
+++ Middleware/Actions/github.js
@@ -7,15 +7,15 @@ import {allDb} from "../../config.js";
 function getWebHooks(req, res) {
     console.log(req.headers['x-github-hook-id'])
     console.log(req.headers)
-    // getOneValueDb(allDb['ActionReaction'], '*', {
-    //     hook_id: req.headers['x-github-hook-id']
-    // })
-    //   .then((res) => {
-    //       console.log(res)
-    //   })
-    //   .catch((err) => {
-    //       console.error(err)
-    //   })
+    getOneValueDb(allDb['ActionReaction'], '*', {
+        hook_id: req.headers['x-github-hook-id']
+    })
+      .then((res) => {
+          console.log(res)
+      })
+      .catch((err) => {
+          console.error(err)
+      })
     res.status(200).send({})
 }
 
