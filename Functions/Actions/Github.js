c913544ca2740a6d36ac508f1ba6c3873375e9a1
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-01 18:26:28 +0100

    [ADD] global actions

--- Functions/Actions/Github.js
+++ Functions/Actions/Github.js
@@ -18,8 +18,7 @@ async function createGithubAction(options) {
       {
         Authorization: `token ${options.token.toString()}`
       })
-    console.log("result =", result)
-    return result.id
+    return result.data.id
   } catch (err) {
     console.error("Err Create Github Action", err)
     throw err;
