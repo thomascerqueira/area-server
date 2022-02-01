5c8adbadfb3cbc309668dc7c2acddde9e377d1e0
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-02-01 10:50:47 +0100

    [UPDATE] confirm mail template

--- Middleware/Auth/auth.js
+++ Middleware/Auth/auth.js
@@ -4,9 +4,9 @@ import {allDb} from "../../config.js";
 import {getOneValueDb} from "../../Functions/MongoDB/getValueDb.js";
 import {sendMail} from '../../Functions/sendMail.js'
 import {userSchema} from "../../user.js";
+import fs from "fs"
 
-const fs = require('fs')
-const template = fs.readFileSync('../../Template/confirm.html',{encoding:'utf-8'});
+const template = fs.readFileSync(__dirname + '../../Template/confirm.html',{encoding:'utf-8'});
 
 function createUser(req, res) {
 
