470736b6b3c3a34cf35823a8099acd91e6f589d0
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-01 16:52:51 +0100

    [MODIF] github create action

--- Functions/httpRequest.js
+++ Functions/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default async function httpRequest(url, method="get", data={}, headers={
-  'Accept': 'application/json'
+  'Content-Type': 'application/json'
 }) {
   try {
     return await axios( {
