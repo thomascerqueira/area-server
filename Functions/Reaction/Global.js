4bba384671288696d9f83cbb0f533360bc5a0619
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-02-05 01:24:49 +0100

    [UPDATE] log payload

--- Functions/Reaction/Global.js
+++ Functions/Reaction/Global.js
@@ -3,6 +3,7 @@ import { sendMail } from "../sendMail.js";
 const reaction = {"send_mail": sendMail}
 
 async function dispatchReaction(payload) {
+    console.log(payload)
     reaction[payload.reaction.reactionName](payload.reaction.data)
 }
 
