4a449d940f79294480f9020ae231c514d5aa911c
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-21 16:00:30 +0100

    [MODIF] jen sais rien

--- Functions/createOurServices.js
+++ Functions/createOurServices.js
@@ -84,7 +84,7 @@ function createJsonServices() {
 
   const data = JSON.stringify(ourServices)
 
-  fs.writeFile('service.json', data, (err => {
+  fs.writeFile('./service.json', data, (err => {
     if (err) {
       console.log(err)
     }
--- Middleware/Services/services.js
+++ Middleware/Services/services.js
@@ -8,6 +8,7 @@ async function updateServices(req, res) {
   const dbRef = db.collection("Services")
 
   file.forEach((serv) => {
+    console.log(serv)
     try {
       dbRef.doc(serv.name).set({
         actions: serv.actions ? serv.actions : [],
