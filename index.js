e086759d5ed44b15604130bb54e6ec09b2c562c5
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-21 16:12:03 +0100

    [MODIF] test cron 1min

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-nodeCron.schedule('*/5 * * * *', async () => {
+nodeCron.schedule('* * * * *', async () => {
   /*
   ** get id list
   ** tableau de fonction => bonne action
