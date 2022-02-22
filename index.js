69faa07f07fca8829a4d6c9eaaec51e0f31facc0
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-02-22 11:35:03 +0100

    [UPDATE] index.js => testing getting data from firebase db

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-nodeCron.schedule('* * * * *', async () => {
+nodeCron.schedule('* * * * *', () => {
   const db = admin.firestore()
   const dbRef = db.collection("Refenreces")
 
