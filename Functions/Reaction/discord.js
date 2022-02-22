9dd04aefd07c500876846661104bc7a7ae83d753
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-02-22 16:05:28 +0100

    [UPDATE] discord.js

---
+++ Functions/Reaction/discord.js
@@ -0,0 +1,49 @@
+import httpRequest from "../httpRequest";
+
+async function createDiscordReaction(options) {
+	try {
+		let result = await httpRequest(
+			`https://discord.com/api/channels/${options.channel_id.toString()}/webhooks`, 'post'
+		)
+		return options
+	} catch (err) {
+		throw err
+	}
+}
+
+async function updateDiscordReaction(options) {
+	try {
+		let result = await httpRequest(
+			`https://discord.com/api/webhooks/${options.webhooks_id.toString()}`, 'patch'
+		)
+	} catch (err) {
+		throw err
+	}
+}
+
+async function deleteDiscordReaction(options) {
+	try {
+		let result = await httpRequest(
+			`https://discord.com/api/webhooks/${options.webhooks_id.toString()}`, 'delete'
+		)
+	} catch (err) {
+		throw err
+	}
+}
+
+async function executeDiscordReaction(options) {
+	try {
+		let result = await httpRequest(
+			`https://discord.com/api/webhooks/${options.webhooks_id.toString()}/${options.webhooks_token}`, 'post'
+		)
+	} catch (err) {
+		throw err
+	}
+}
+
+export {
+    createDiscordReaction,
+	updateDiscordReaction,
+	deleteDiscordReaction,
+	executeDiscordReaction,
+}
