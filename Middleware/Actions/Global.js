37d867193f71474361de40601ee9e1d1429e79e5
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-23 12:43:31 +0100

    [MODIF] modif try catch for create action

--- Middleware/Actions/Global.js
+++ Middleware/Actions/Global.js
@@ -6,54 +6,52 @@ import generateID from "../../Functions/generateID.js";
 const actions = {'push': createGithubAction}
 
 async function createActionReaction(req, res) {
+  let token
   try {
-    let token
-    try {
-      token = req.body.tokenID.split(' ')[1]
-    } catch (err) {
-      res.status(500).send({'msg': "Bad format Token"})
-      return
-    }
+    token = req.body.tokenID.split(' ')[1]
+  } catch (err) {
+    res.status(500).send({'msg': "Bad format Token"})
+    return
+  }
 
-    auth.verifyIdToken(token)
-      .then(async (decoded) => {
-        const data = await actions[req.body.action.actionName](req.body.action.data)
-        const id = generateID()
-        addDocC(
-          allDb["ActionReaction"], "ActionReaction", {
-            "uid": decoded.user_id,
-            "id": id,
-            "action": {
-              "service": req.body.action.service.toString(),
-              "actionName": req.body.action.actionName.toString(),
-              "data": data
-            },
-            "reaction": {
-              "service": req.body.reaction.service.toString(),
-              "reactionName": req.body.reaction.reactionName.toString(),
-              "data": req.body.reaction.data
-            }
-          })
-          .then((result) => {
-            res.status(200).send(result)
-          })
+  auth.verifyIdToken(token)
+    .then(async (decoded) => {
+      let data
+      try {
+        await actions[req.body.action.actionName](req.body.action.data)
+      } catch (err) {
+        console.error(err)
+        res.status(500).send({'msg': "unknown action name"})
+        return
+      }
+      const id = generateID()
+      addDocC(
+        allDb["ActionReaction"], "ActionReaction", {
+          "uid": decoded.user_id,
+          "id": id,
+          "action": {
+            "service": req.body.action.service.toString(),
+            "actionName": req.body.action.actionName.toString(),
+            "data": data
+          },
+          "reaction": {
+            "service": req.body.reaction.service.toString(),
+            "reactionName": req.body.reaction.reactionName.toString(),
+            "data": req.body.reaction.data
+          }
+        })
+        .then((result) => {
+          res.status(200).send(result)
+        })
         .catch((err) => {
           console.error("Add doc", err)
-          throw err
+          res.status(500).send(err)
         })
-      })
+    })
     .catch((err) => {
       console.error("token invalid ", err)
-      throw err
-    })
-  }catch(err) {
-    try {
-      console.error("global error", err)
-      res.status(500).send({msg: err})
-    } catch (err) {
       res.status(500).send(err)
-    }
-  }
+    })
 }
 
 export {
