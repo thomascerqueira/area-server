e9be019e06ab3a868e1c2378ff6e118f5930c94e
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-27 14:20:59 +0100

    [TEST] Doesnt take the name in consideration

--- Functions/FIrebase.js
+++ Functions/FIrebase.js
@@ -4,10 +4,11 @@ function addValueArray(nameCollection, nameDoc, nameArray, data) {
   const db = admin.firestore()
   const dbRef = db.collection(nameCollection).doc(nameDoc)
 
+  const newData = {}
+  newData[nameArray] = admin.firestore.FieldValue.arrayUnion(data)
+
   console.log("data ", data)
-  dbRef.update({
-    nameArray: admin.firestore.FieldValue.arrayUnion(data)
-  })
+  dbRef.update(newData)
     .then(() => {})
     .catch(err => {
       throw err
