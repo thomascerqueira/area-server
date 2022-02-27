b365ee82d45c0f9d803f72e6b2780fda902ff894
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-27 14:55:53 +0100

    [TEST] Case weather action temp

--- Functions/Actions/Weather.js
+++ Functions/Actions/Weather.js
@@ -6,7 +6,25 @@ async function weatherActionTemp(data) {
       `http://api.openweathermap.org/data/2.5/weather?q=${data['city']}&appid=${process.env.OPEN_WEATHER_KEY}`,
       "get"
     )
-    console.log(result)
+    switch (data['option']) {
+      case 'less':
+        if (result.data.main['temp'] <= data['temp']) {
+          return true;
+        }
+        break
+      case 'more':
+        if (result.data.main['temp'] >= data['temp']) {
+          return true;
+        }
+        break
+      case 'equal':
+        if (result.data.main['temp'] === data['temp']) {
+          return true;
+        }
+        break;
+      default:
+        return false
+    }
     return false;
   } catch (e) {
     console.error("Err create Weather Temp action", e)
