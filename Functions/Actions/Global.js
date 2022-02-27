215206c75fe92123976ddb267f833a3ec23c7971
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-27 14:17:37 +0100

    [TEST] Forgot the s

--- Functions/Actions/Global.js
+++ Functions/Actions/Global.js
@@ -2,7 +2,7 @@ import {addValueArray} from "../FIrebase.js";
 
 function createSurveyAction(_, id) {
   try {
-    addValueArray("Reference", "Surveys", "id_survey", id)
+    addValueArray("References", "Surveys", "id_survey", id)
   } catch (e) {
     throw e
   }
