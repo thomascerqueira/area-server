89807ec7c4b34a86778e4748be1e59e3e502c650
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-27 14:14:22 +0100

    [TEST] Put the good name

--- Functions/Actions/Global.js
+++ Functions/Actions/Global.js
@@ -2,7 +2,7 @@ import {addValueArray} from "../FIrebase.js";
 
 function createSurveyAction(_, id) {
   try {
-    addValueArray("Reference", "Surveys", nameArray, id)
+    addValueArray("Reference", "Surveys", "id_survey", id)
   } catch (e) {
     throw e
   }
