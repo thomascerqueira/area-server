38185069c762f496c70011d6a4528f1388d7e937
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-27 14:47:05 +0100

    [TEST] Forgot http

--- Functions/Actions/Weather.js
+++ Functions/Actions/Weather.js
@@ -3,7 +3,7 @@ import httpRequest from '../httpRequest.js';
 async function weatherActionTemp(data) {
   try {
     let result = await httpRequest(
-      `api.openweathermap.org/data/2.5/weather?q=${data['city']}&appid=${process.env.OPEN_WEATHER_KEY}`,
+      `http://api.openweathermap.org/data/2.5/weather?q=${data['city']}&appid=${process.env.OPEN_WEATHER_KEY}`,
       "get"
     )
     console.log(result)
