91d1b85c58463e0d895b5ee9d812c8c3c0aaadef
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-02-27 16:47:40 +0100

    [MODIF] Its not delete a services but delete an ActionReaction

---
+++ Middleware/Actions/Delete.js
@@ -0,0 +1,44 @@
+import {allDb, auth} from "../../config.js";
+import {dropDocument} from "../../Functions/MongoDB/dropCollection.js";
+import {removeValueArray} from "../../Functions/FIrebase.js";
+
+function deleteActionReaction(req, res) {
+  let token
+  try {
+    token = req.headers.tokenid.split(' ')[1]
+  } catch (err) {
+    console.error(err)
+    res.status(401).send({'msg': "Bad format Token"})
+    return
+  }
+
+  auth.verifyIdToken(token)
+    .then((decoded) => {
+      console.log(`Deleting ActionReaction ${req.body.id} for user ${decoded.uid}`)
+      dropDocument(allDb['ActionReaction'], 'ActionReaction', {
+        id: req.body.id,
+        uid: decoded.uid
+      }).then(() => {
+        try {
+          removeValueArray("References", "Surveys", "id_survey", req.body.id)
+          res.status(200).send({'msg': 'Delete success'})
+          console.log(`ActionReaction ${req.body.id} Deleted successfully`)
+        } catch (err) {
+          console.error(err)
+          res.status(500).send(err)
+        }
+      })
+        .catch((err) => {
+          console.error(err)
+          res.status(500).send(err)
+        })
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(401).send(err);
+    })
+}
+
+export {
+  deleteActionReaction
+}
--- Middleware/Services/services.js
+++ Middleware/Services/services.js
@@ -89,46 +89,8 @@ function getServicesUser(req, res) {
     })
 }
 
-function deleteService(req, res) {
-  let token
-  try {
-    token = req.headers.tokenid.split(' ')[1]
-  } catch (err) {
-    console.error(err)
-    res.status(401).send({'msg': "Bad format Token"})
-    return
-  }
-
-  auth.verifyIdToken(token)
-    .then((decoded) => {
-      console.log(`Deleting ActionReaction ${req.body.id} for user ${decoded.uid}`)
-      dropDocument(allDb['ActionReaction'], 'ActionReaction', {
-        id: req.body.id,
-        uid: decoded.uid
-      }).then(() => {
-        try {
-          removeValueArray("References", "Surveys", "id_survey", req.body.id)
-          res.status(200).send({'msg': 'Delete success'})
-          console.log(`ActionReaction ${req.body.id} Deleted successfully`)
-        } catch (err) {
-          console.error(err)
-          res.status(500).send(err)
-        }
-      })
-        .catch((err) => {
-          console.error(err)
-          res.status(500).send(err)
-        })
-    })
-    .catch((err) => {
-      console.error(err)
-      res.status(401).send(err);
-    })
-}
-
 export {
   getAllServices,
   getServicesUser,
   updateServices,
-  deleteService
 }
