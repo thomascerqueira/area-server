7d311b5900463bf949c088b46cac19cde3579183
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-02-28 01:50:55 +0100

    [UPDATE] global creation

--- Middleware/Actions/Global.js
+++ Middleware/Actions/Global.js
@@ -1,11 +1,11 @@
-import {createGithubAction} from "../../Functions/Actions/Github.js";
-import {addDocC} from "../../Functions/MongoDB/addDoc.js";
-import {allDb, auth} from "../../config.js";
+import { createGithubAction } from "../../Functions/Actions/Github.js";
+import { addDocC } from "../../Functions/MongoDB/addDoc.js";
+import { allDb, auth } from "../../config.js";
 import generateID from "../../Functions/generateID.js";
 import { createDiscordReaction } from "../../Functions/Reaction/discord.js";
 
-const actions = {'push': createGithubAction}
-const reactions = {'sendMessage': createDiscordReaction}
+const actions = { 'push': createGithubAction }
+const reactions = { 'sendMessage': createDiscordReaction }
 
 async function createActionReaction(req, res) {
   try {
@@ -14,39 +14,39 @@ async function createActionReaction(req, res) {
     auth.verifyIdToken(token)
       .then(async (decoded) => {
         const actionData = await actions[req.body.action.actionName](req.body.action.data)
-        const reactionData = await reactions[req.body.action.reactionName](req.body.reaction.data)
+        const reactionData = await reactions[req.body.reaction.reactionName](req.body.reaction.data)
         const id = generateID()
         addDocC(
           allDb["ActionReaction"], "ActionReaction", {
-            "uid": decoded.user_id,
-            "id": id,
-            "action": {
-              "service": req.body.action.service.toString(),
-              "actionName": req.body.action.actionName.toString(),
-              "data": data
-            },
-            "reaction": {
-              "service": req.body.reaction.service.toString(),
-              "reactionName": req.body.reaction.reactionName.toString(),
-              "data": req.body.reaction.data
-            }
-          })
+          "uid": decoded.user_id,
+          "id": id,
+          "action": {
+            "service": req.body.action.service.toString(),
+            "actionName": req.body.action.actionName.toString(),
+            "data": data
+          },
+          "reaction": {
+            "service": req.body.reaction.service.toString(),
+            "reactionName": req.body.reaction.reactionName.toString(),
+            "data": req.body.reaction.data
+          }
+        })
           .then((result) => {
             res.status(200).send(result)
           })
-        .catch((err) => {
-          console.error("Add doc", err)
-          throw err
-        })
+          .catch((err) => {
+            console.error("Add doc", err)
+            throw err
+          })
       })
-    .catch((err) => {
-      console.error("token invalid ", err)
-      throw err
-    })
-  }catch(err) {
+      .catch((err) => {
+        console.error("token invalid ", err)
+        throw err
+      })
+  } catch (err) {
     try {
       console.error("global error", err)
-      res.status(500).send({msg: err})
+      res.status(500).send({ msg: err })
     } catch (err) {
       res.status(500).send(err)
     }
@@ -54,15 +54,15 @@ async function createActionReaction(req, res) {
 }
 
 async function getActionReaction(req, res) {
-  res.status(401).send({msg: "not implemented"})
+  res.status(401).send({ msg: "not implemented" })
 }
 
 async function updateActionReaction(req, res) {
-  res.status(401).send({msg: "not implemented"})
+  res.status(401).send({ msg: "not implemented" })
 }
 
 async function deleteActionReaction(req, res) {
-  res.status(401).send({msg: "not implemented"})
+  res.status(401).send({ msg: "not implemented" })
 }
 
 export {
