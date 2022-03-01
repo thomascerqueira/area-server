c65c07702fe488523581f75ee2f8c27b37f80b37
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-01 23:42:00 +0100

    [ADD] test custom validator

--- Routes/Auth/auth.js
+++ Routes/Auth/auth.js
@@ -1,5 +1,6 @@
 import { createUser, deleteUser, signUser, signUserProvider } from '../../Middleware/Auth/auth.js'
-import {create_route_from_routes, check_arg, check_header} from '../../Functions/createroutefromroutes.js'
+import { create_route_from_routes, check_arg, check_header } from '../../Functions/createroutefromroutes.js'
+import { checkSchema, validationResult } from 'express-validator'
 
 const routes = [
     {
@@ -25,9 +26,38 @@ const routes = [
         route: '/signUserProvider',
         middlewares: [check_header(['tokenid']), check_arg(['user'])],
         callback: signUserProvider
+    },
+    {
+        type: 'post',
+        route: '/test',
+        middlewares: [checkSchema({
+            email: {
+                in: ['body'],
+                errorMessage: "Email is missing",
+                isEmail: true,
+            },
+            password: {
+                in: ["body"],
+                errorMessage: "Password is missing",
+                isStrongPassword: true
+            },
+            username: {
+                in: ["body"],
+                errorMessage: "Username is missing",
+            }
+        }), checkValidator()]
     }
 ]
 
-
+function checkValidator() {
+    return (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(500).send({ errors: errors.array() })
+            return
+        }
+        next()
+    }
+}
 
 export default create_route_from_routes(routes)
