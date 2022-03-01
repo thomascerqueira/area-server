a6ae4f3e1dadf1447b6e4afee6bde6df68d53101
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-03-01 17:42:50 +0100

    [MODIF] Console weather

--- Functions/Actions/Weather.js
+++ Functions/Actions/Weather.js
@@ -31,7 +31,6 @@ async function weatherActionTemp(data) {
       `http://api.openweathermap.org/data/2.5/weather?q=${data['city']}&appid=${process.env.OPEN_WEATHER_KEY}&units=metric`,
       "get"
     )
-    console.log(result.data)
     return checkOption(data['option'], result.data.main['temp'], data['temperature'])
   } catch (e) {
     console.error("Err in Weather Temp action", e)
