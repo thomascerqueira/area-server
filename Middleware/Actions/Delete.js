9e2f17d058c58cfaa6a5cc56d6011d5693acdfda
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-02 14:52:33 +0100

    [UPDATE] reserved word

--- Middleware/Actions/Delete.js
+++ Middleware/Actions/Delete.js
@@ -36,7 +36,7 @@ function deleteActionReaction(req, res) {
       getOneValueDb(allDb['ActionReaction'], "ActionReaction", {
         id: req.body.id,
         uid: decoded.uid
-      }).then((res) => {
+      }).then(async (res) => {
         try {
           await action[req.action.actionName](req.action.data)
           await reaction[req.reaction.reactionName](req.action.data)
