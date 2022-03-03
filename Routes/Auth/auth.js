7e220024fdeeb2c80b3799ec9fa7f55cd3a79071
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-03 03:58:04 +0100

    [ADD] gettokenroutes

--- Routes/Auth/auth.js
+++ Routes/Auth/auth.js
@@ -41,10 +41,10 @@ async function getGitHubToken(req, res) {
             "https://github.com/login/oauth/access_token",
             "post",
             {
-                "client_id": "b8b149a225608f23c2b6",
-                "client_secret": "d91aafb434103b6f5c400e5294fb4292900acecd",
-                "redirect_url": "http://localhost:3000/services",
-                "code": `${req.body.code}`
+                client_id: "b8b149a225608f23c2b6",
+                client_secret: "d91aafb434103b6f5c400e5294fb4292900acecd",
+                redirect_url: "http://localhost:3000/services",
+                code: `${req.body.code}`
             }
         )
         res.status(200).send(result)
