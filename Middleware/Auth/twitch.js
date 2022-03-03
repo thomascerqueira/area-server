9d39394818e3a1e06c57fb67787fa4759e4b832d
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-03 19:02:58 +0100

    [TRY] get access token from server

--- Middleware/Auth/twitch.js
+++ Middleware/Auth/twitch.js
@@ -2,13 +2,13 @@ import httpRequest from "../../Functions/httpRequest.js"
 
 function getTwitchAccessToken(req, res) {
     const code = req.body.code
-    httpRequest("https://id.twitch.tv/oauth2/token", {
+    httpRequest("https://id.twitch.tv/oauth2/token", "post", {
         client_id: "iar5wg2v3xoe7mzbds9sgx16mvkabi",
         client_secret: "4cs3b9tt6usygjqkytmt4qgbghd4vg",
         code: `${code}`,
         grant_type: "authorization_code",
         redirect_uri: "http://localhost:3000/services"
-    }, "post")
+    })
         .then((response) => {
             res.status(200).send(response)
         })
