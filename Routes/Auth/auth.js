285026d8f37467b13ae281f11c01cf2e4b7c943d
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-03 04:01:31 +0100

    [ADD] gettokenroutes

--- Routes/Auth/auth.js
+++ Routes/Auth/auth.js
@@ -3,56 +3,57 @@ import { create_route_from_routes, check_arg, check_header } from '../../Functio
 import httpRequest from '../../Functions/httpRequest.js'
 
 const routes = [
-    {
-        type: 'post',
-        route: '/createUser',
-        middlewares: [check_arg(['email', 'password', 'username'])],
-        callback: createUser
-    },
-    {
-        type: 'delete',
-        route: '/deleteUser',
-        middlewares: [check_header(['tokenid'])],
-        callback: deleteUser
-    },
-    {
-        type: 'post',
-        route: '/signUser',
-        middlewares: [check_header(['tokenid'])],
-        callback: signUser
-    },
-    {
-        type: 'post',
-        route: '/signUserProvider',
-        middlewares: [check_header(['tokenid']), check_arg(['user'])],
-        callback: signUserProvider
-    },
-    {
-        type: 'post',
-        route: "/getGitHubToken",
-        middlewares: [],
-        callback: getGitHubToken
-    }
+	{
+		type: 'post',
+		route: '/createUser',
+		middlewares: [check_arg(['email', 'password', 'username'])],
+		callback: createUser
+	},
+	{
+		type: 'delete',
+		route: '/deleteUser',
+		middlewares: [check_header(['tokenid'])],
+		callback: deleteUser
+	},
+	{
+		type: 'post',
+		route: '/signUser',
+		middlewares: [check_header(['tokenid'])],
+		callback: signUser
+	},
+	{
+		type: 'post',
+		route: '/signUserProvider',
+		middlewares: [check_header(['tokenid']), check_arg(['user'])],
+		callback: signUserProvider
+	},
+	{
+		type: 'post',
+		route: "/getGitHubToken",
+		middlewares: [],
+		callback: getGitHubToken
+	}
 ]
 
 async function getGitHubToken(req, res) {
-    try {
-        let result = await httpRequest(
-            "https://github.com/login/oauth/access_token",
-            "post",
-            {
-                client_id: "b8b149a225608f23c2b6",
-                client_secret: "d91aafb434103b6f5c400e5294fb4292900acecd",
-                redirect_url: "http://localhost:3000/services",
-                code: `${req.body.code}`
-            }
-        )
-        res.status(200).send(result)
-    } catch (err) {
-        console.log(err)
-        res.status(401).send({ msg: err })
-        return
-    }
+	try {
+		let result = await httpRequest(
+			"https://github.com/login/oauth/access_token",
+			"post",
+			{
+				client_id: "b8b149a225608f23c2b6",
+				client_secret: "d91aafb434103b6f5c400e5294fb4292900acecd",
+				redirect_url: "http://localhost:3000/services",
+				code: `${req.body.code}`
+			}
+		)
+		console.log(result)
+		res.status(200).send(result)
+	} catch (err) {
+		console.log(err)
+		res.status(401).send({ msg: err })
+		return
+	}
 }
 
 export default create_route_from_routes(routes)
