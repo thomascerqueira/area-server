1c5ed98e982e640a325cd252dd8b7ae1868fc087
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-03 20:39:06 +0100

    [UPDATE] github access token handling

--- Middleware/Auth/github.js
+++ Middleware/Auth/github.js
@@ -3,12 +3,12 @@ import httpRequest from "../../Functions/httpRequest.js"
 function getGithubAccessToken(req, res) {
 	const code = req.body.code
 	console.log(code)
-	httpRequest("https://github.com/login/oauth/access_token", "post",
+	httpRequest(`https://github.com/login/oauth/access_token?client_id=b8b149a225608f23c2b6&client_secret=d91aafb434103b6f5c400e5294fb4292900acecd&code=${code}&redirect_uri=http://localhost:3000/services`, "post",
 		{
-			client_id: "b8b149a225608f23c2b6",
-			client_secret: "d91aafb434103b6f5c400e5294fb4292900acecd",
-			code: code,
-			redirect_uri: "http://localhost:3000/services",
+			// client_id: "b8b149a225608f23c2b6",
+			// client_secret: "d91aafb434103b6f5c400e5294fb4292900acecd",
+			// code: code,
+			// redirect_uri: "http://localhost:3000/services",
 		},
 		{
 			"Accept": "application/json",
