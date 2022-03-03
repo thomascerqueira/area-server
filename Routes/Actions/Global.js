6396b7e410c5f226b6913c2b6541006fd64d18b0
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-03 03:27:15 +0100

    [ADD] gettokenroutes

--- Routes/Actions/Global.js
+++ Routes/Actions/Global.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {create_route_from_routes, check_arg, check_header} from '../../Functions/createroutefromroutes.js'
-import {createActionReaction, getSurveyAction, updateSurveyAction} from "../../Middleware/Actions/Global.js";
-import {testCovid} from "../../Middleware/Actions/Covid.js";
-import {deleteActionReaction, deleteForced} from "../../Middleware/Actions/Delete.js";
+import { create_route_from_routes, check_arg, check_header } from '../../Functions/createroutefromroutes.js'
+import { createActionReaction, getSurveyAction, updateSurveyAction } from "../../Middleware/Actions/Global.js";
+import { testCovid } from "../../Middleware/Actions/Covid.js";
+import { deleteActionReaction, deleteForced } from "../../Middleware/Actions/Delete.js";
+import httpRequest from '../../Functions/httpRequest.js';
 
 const routes = [
   {
@@ -40,7 +41,25 @@ const routes = [
     route: "/testCovid",
     middlewares: [check_arg(['country', 'iso'])],
     callback: testCovid
+  },
+  {
+    type: 'post',
+    route: "/getGitHubToken",
+    middlewares: [],
+    callback: getGitHubToken
   }
 ]
+
+async function getGitHubToken(res, res) {
+  fetch("https://github.com/login/oauth/access_token", {
+      method: "post",
+      body: new URLSearchParams({
+        client_id: "b8b149a225608f23c2b6",
+        client_secret: "d91aafb434103b6f5c400e5294fb4292900acecd",
+        code: req.body.code,
+        redirect_uri: "http://localhost:3000/services",
+      })
+    }).then(response => res.send(response))
+}
 
 export default create_route_from_routes(routes)
