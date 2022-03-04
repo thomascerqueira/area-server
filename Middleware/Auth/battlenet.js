77f036ea06df1eac061bb4a51a9078c07c411d6a
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-04 01:17:30 +0100

    [UPDATE] battlenet access token handling

--- Middleware/Auth/battlenet.js
+++ Middleware/Auth/battlenet.js
@@ -2,14 +2,10 @@ import httpRequest from "../../Functions/httpRequest.js"
 
 function getBattleNetAccessToken(req, res) {
 	const code = req.body.code
-	httpRequest(`https://eu.battle.net/oauth/token`, "post",
+	httpRequest(`https://eu.battle.net/oauth/token?code=${code}&grant_type=authorization_code&redirect_uri=https://localhost:3000/services`, "post",
 		{
-			code: code,
-			grant_type: "authorization_code",
-			redirect_uri: "https://localhost:3000/services",
 		},
 		{
-			"Content-Type": "application/x-www-form-urlencoded",
 			"Authorization": "Basic YTI5ZGZmYzMwY2JmNDUxMmE0MTg3NDBkNWVkODRlZjA6VW5JajZmeXZBUWZHTUpvN29KVzE4bTNHWExpVE9DclI="
 		}).then((response) => {
 			console.log(response)
