5c2b071fa3ce2d78eecfade4f53d9f0f460066ab
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-04 16:28:25 +0100

    [UPDATE] spotify access token handling

--- Middleware/Auth/spotify.js
+++ Middleware/Auth/spotify.js
@@ -2,11 +2,8 @@ import httpRequest from "../../Functions/httpRequest.js"
 
 function getSpotifyAccessToken(req, res) {
 	const code = req.body.code
-	httpRequest("https://accounts.spotify.com/api/token", "post",
+	httpRequest(`https://accounts.spotify.com/api/token?grant_type=authorization_code&code=${code}&redirect_uri=http://localhost:3000/services`, "post",
 		{
-			grant_type: "authorization_code",
-			code: code,
-			redirect_uri: "http://localhost:3000/services",
 		},
 		{
 			'Content-Type': 'application/x-www-form-urlencoded',
