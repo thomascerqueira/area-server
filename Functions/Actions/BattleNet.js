9768b9f391d3dd105ceb17a5f8a0786ef7667af2
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-05 19:26:54 +0100

    [UPDATE] battle net action

--- Functions/Actions/BattleNet.js
+++ Functions/Actions/BattleNet.js
@@ -4,32 +4,29 @@ import { checkOption } from "./Weather.js";
 import {getOneValueDb} from "../MongoDB/getValueDb.js";
 
 async function battleNetAction(data, uid) {
-    try {
-        const data = await getOneValueDb(allDb['UsersDB'], 'users', {
-            uid: uid
-        })
-        const token = data['services']['battleNet']['token']
+	try {
+		const data = await getOneValueDb(allDb['UsersDB'], 'users', {
+			uid: uid
+		})
+		const token = data['services']['battleNet']['token']
 
-        let result = await httpRequest(
-            `https://eu.api.blizzard.com/data/wow/token/index?namespace=dynamic-eu&locale=fr_FR`,
-            "get",
-            {},
-            {
-                "Authorization": `Bearer ${token}`
-            }
-        )
-        console.log(result)
-        if (result.data.length > 0) {
-            console.log(result.data['price'])
-            console.log(data['option'])
-            console.log(data['value'])
-            return (checkOption(data['option'], result.data['price'], data['value']))
-        }
-        return false
-    } catch (e) {
-        console.error("Err in Battle Net action", e)
-        throw e
-    }
+		let result = await httpRequest(
+			`https://eu.api.blizzard.com/data/wow/token/index?namespace=dynamic-eu&locale=fr_FR`,
+			"get",
+			{},
+			{
+				"Authorization": `Bearer ${token}`
+			}
+		)
+		console.log(result)
+		console.log(result.data['price'])
+		console.log(data['option'])
+		console.log(data['value'])
+		return (checkOption(data['option'], result.data['price'], data['value']))
+	} catch (e) {
+		console.error("Err in Battle Net action", e)
+		throw e
+	}
 }
 
 export {
