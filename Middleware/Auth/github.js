9c8598f3e72589e668436e8d6984121ad7f0f3c5
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-03-05 17:36:53 +0100

    [MODIF] only show data

--- Middleware/Auth/github.js
+++ Middleware/Auth/github.js
@@ -17,15 +17,15 @@ function getGithubAccessToken(req, res) {
         updateDoc(allDb[process.env.DB_MONGO_USERS], 'users', {
           uid: decoded.uid
         }, {
-          $set: {
             services: {
-              github: {
-                token: response.data.access_token,
-                refresh_token: "",
-                connected: true
+              $set: {
+                github: {
+                  token: response.data.access_token,
+                  refresh_token: "",
+                  connected: true
+                }
               }
             }
-          }
         })
           .then(() => {
             console.log(response.data)
