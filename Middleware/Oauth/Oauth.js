38bf819e463b94a3ede051f1400c28684d8a4f86
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-03-05 21:17:06 +0100

    [MODIF] absoulte path

--- Middleware/Oauth/Oauth.js
+++ Middleware/Oauth/Oauth.js
@@ -2,7 +2,7 @@ import {auth} from "../../config.js";
 
 function authorize(req, res) {
       console.log(req.query.redirect_uri)
-      res.status(200).render('../../Template/Oauth')
+      res.status(200).render('./Template/Oauth')
 }
 
 export {
