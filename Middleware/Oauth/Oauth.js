2d8a7003af826895e3c856815614be61d2fb753d
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-03-05 21:33:03 +0100

    [MODIF] views

--- Middleware/Oauth/Oauth.js
+++ Middleware/Oauth/Oauth.js
@@ -2,7 +2,7 @@ import {auth} from "../../config.js";
 
 function authorize(req, res) {
       console.log(req.query.redirect_uri)
-      res.status(200).render('Oauth')
+      res.status(200).render('Oauth', {test: "Quest ce que tu fais"})
 }
 
 export {
