c71f4deb7a38f7f0f123ff7f23160ec2ab3f789a
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2022-03-05 21:42:47 +0100

    [MODIF] test fonction

--- Middleware/Oauth/Oauth.js
+++ Middleware/Oauth/Oauth.js
@@ -2,7 +2,7 @@ import {auth} from "../../config.js";
 
 function authorize(req, res) {
       console.log(req.query.redirect_uri)
-      res.status(200).render('Oauth', {test: "Quest ce que tu fais"})
+      res.status(200).render('Oauth', {redirect_uri: `${req.query.redirect_uri}`})
 }
 
 export {
