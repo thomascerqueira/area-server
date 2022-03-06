c458ef7b79a7b3ac897203b93cc5d301a466c5b7
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2022-03-06 01:54:44 +0100

    [UPDATE] coinranking

--- Functions/Actions/Coinranking.js
+++ Functions/Actions/Coinranking.js
@@ -21,6 +21,7 @@ async function coinrankingGetUuid(name) {
 async function coinrankingGetCoin(data) {
   try {
     let result = await coinrankingGetUuid(data['name'])
+    console.log(result)
     const uuid = result.data.uuid
     result = await httpRequest(
       `https://api.coinranking.com/v2/coin/${uuid}/price`,
